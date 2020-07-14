2d90ac46e50d53e4bfa6ebdca4141da77da9e9b9
Devon Navon <devon.navon@gmail.com>
2020-07-14 15:05:05 -0700

    login token saved in local storage, need to emit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
 					<v-col cols="12" sm="8" md="4">
 						<v-card class="elevation-12">
 							<v-toolbar color="primary" dark flat>
-								<v-toolbar-title>Login form</v-toolbar-title>
+								<v-toolbar-title>Login to the B L o G</v-toolbar-title>
 								<v-spacer></v-spacer>
 								<!-- <v-tooltip bottom>
 									<template v-slot:activator="{ on }">
@@ -21,9 +21,10 @@
 								<v-form>
 									<v-text-field
 										label="Login"
-										name="login"
+										name="username"
 										prepend-icon="mdi-account"
 										type="text"
+										v-model="username"
 									></v-text-field>
 
 									<v-text-field
@@ -32,12 +33,14 @@
 										name="password"
 										prepend-icon="mdi-lock"
 										type="password"
+										v-model="password"
+										v-on:keyup.enter="loginUser"
 									></v-text-field>
 								</v-form>
 							</v-card-text>
 							<v-card-actions>
 								<v-spacer></v-spacer>
-								<v-btn color="primary" id="login" @click="getUser">Login</v-btn>
+								<v-btn color="primary" @click.prevent="loginUser">Login</v-btn>
 								<v-btn color="primary">Sign Up</v-btn>
 							</v-card-actions>
 						</v-card>
@@ -53,6 +56,38 @@ export default {
 	name: 'Login',
 	props: {
 		source: String,
+	},
+	data: () => ({
+		username: null,
+		password: null,
+		error: {
+			isError: false,
+			message: 'Invalid Login',
+		},
+	}),
+	methods: {
+		async postData(url = '', data = {}) {
+			const response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			});
+			return response.json();
+		},
+		async loginUser() {
+			try {
+				let r = await this.postData(`${process.env.VUE_APP_API_URL}/login`, {
+					username: this.username,
+					password: this.password,
+				});
+				localStorage.setItem('token', r.token);
+				console.log(r);
+			} catch (err) {
+				console.log(err);
+			}
+		},
 	},
 };
 </script>
